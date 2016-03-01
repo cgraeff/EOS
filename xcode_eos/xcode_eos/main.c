9e2c9393b7494810d981d7a164f38cf9522c3163
Clebson Graeff <clebsongraeff@cliente105-228.wireless.ufsc.br>
2016-03-01 15:23:15 -0300

    Added Xcode project

---
+++ xcode_eos/xcode_eos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  xcode_eos
+//
+//  Created by Clebson Graeff on 3/1/16.
+//  Copyright © 2016 Clebson Graeff. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
