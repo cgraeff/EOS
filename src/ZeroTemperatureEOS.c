cc10532c499df6234409ff3a95f9c0da441cecc5
Clebson Abati Graeff <cgraeff@utfpr.edu.br>
2016-09-04 22:49:31 -0300

    Beta equilibrium initial implementation

--- src/ZeroTemperatureEOS.c
+++ src/ZeroTemperatureEOS.c
@@ -14,12 +14,134 @@
 #include "Constants.h"
 #include "ZeroTemperatureEOS.h"
 
+typedef struct _multi_dim_root_params{
+    double barionic_density;
+} multi_dim_root_params;
+
+void SolveMultiRoots()
+{
+    multi_dim_root_params p;
+    p.barionic_density = barionic_density;
+
+    gsl_multiroot_function f;
+    f.f = &ZeroedGapAndBarionicDensityEquations;
+    f.n = dimension
+    f.params = (void *)&p;
+
+    gsl_vector * initial_guess = gsl_vector_alloc(dimension);
+
+    gsl_vector_set(initial_guess, 0, x_initial_guess);
+    gsl_vector_set(initial_guess, 1, y_initial_guess);
+
+    gsl_vector * solution;
+
+    MultiDimensionalRootFinder(&f,
+                               initial_guess,
+                               solution);
+
+    // The solver creates a vector, free it
+    // so memory don't leak
+    gsl_vector_free(solution);
+
+    *return_mass = x;
+    *return_renormalized_chemical_potential = y;
+}
+
+void MultiDimensionalRootFinder(int dimension
+                                gsl_multiroot_function * f,
+                                gsl_vector * initial_guess,
+                                gsl_vector * solution)
+{
+    int status;
+    size_t iter = 0;
+
+    const gsl_multiroot_fsolver_type * solver_type = gsl_multiroot_fsolver_hybrids;
+    gsl_multiroot_fsolver * solver = gsl_multiroot_fsolver_alloc(solver_type,
+                                                                 dimension);
+
+    gsl_multiroot_fsolver_set(solver, f, initial_guess);
+
+    do {
+        iter++;
+        status = gsl_multiroot_fsolver_iterate(solver);
+
+        if (status == GSL_EBADFUNC){
+            printf("TwodimensionalRootFinder: Error: Infinity or division by zero.\n");
+            abort();
+        }
+        else if (status == GSL_ENOPROG){
+            printf("TwodimensionalRootFinder: Error: Solver is stuck. Try a different initial guess.\n");
+            abort();
+        }
+
+        // Check if the root is good enough:
+        // tests for the convergence of the sequence by comparing the last step dx with the
+        // absolute error epsabs and relative error epsrel to the current position x. The test
+        // returns GSL_SUCCESS if the following condition is achieved,
+        //
+        // |dx_i| < epsabs + epsrel |x_i|
+
+        gsl_vector * x = gsl_multiroot_fsolver_root(solver); // current root
+        gsl_vector * dx = gsl_multiroot_fsolver_dx(solver); // last step
+
+        status = gsl_multiroot_test_delta(dx,
+                                          x,
+                                          parameters.mass_and_renorm_chem_pot_solution_abs_error,
+                                          parameters.mass_and_renorm_chem_pot_solution_rel_error);
+
+    } while (status == GSL_CONTINUE
+             && iter < parameters.mass_and_renor_chem_pot_solution_max_iter);
+
+    solution = gsl_multiroot_fsolver_root(solver);
+
+    gsl_multiroot_fsolver_free(solver);
+    gsl_vector_free (initial_guess);
+
+}
+
+int MultiDimensionalRootFinderHelperFunction(const gsl_vector * x,
+                                             void * p,
+                                             gsl_vector * return_values)
+{
+    multi_dim_root_params * params = (multi_dim_root_params *)p;
+    double barionic_density = params->barionic_density;
+
+   	const double mass = gsl_vector_get(x, 0);
+    const double proton_fraction = gsl_vector(x, 1);
+
+    double proton_density = proton_fraction * barionic_density;
+    double neutron_density = (1.0 - proton_fraction) * barionic_density;
+    double electron_density = proton_density; // Ensures charge neutrality
+
+	double proton_fermi_momentum = CONST_HBAR_C * pow(3.0 * pow(M_PI, 2.0) * proton_density, 1.0 / 3.0);
+	double neutron_fermi_momentum = CONST_HBAR_C * pow(3.0 * pow(M_PI, 2.0) * neutron_density, 1.0 / 3.0);
+    double electron_fermi_momentum = CONST_HBAR_C * pow(3.0 * pow(M_PI, 2.0) * electron_density, 1.0 / 3.0);
+
+	gap_equation_input gap_params;
+    gap_params.proton_fermi_momentum = proton_fermi_momentum;
+    gap_params.neutron_fermi_momentum = neutron_fermi_momentum;
+    gap_params.proton_density;
+    gap_params.neutron_density;
+
+    double zeroed_gap_eq = GapEquation(double mass, &gap_params);
+    double zeroed_beta_equilibrium_rel = neutron_chemical_potential
+                                         - proton_chemical_potential
+                                         - electron_chemical_potential;
+    // Prepare return vector
+   	gsl_vector_set(return_values, 0, zeroed_gap_eq);
+   	gsl_vector_set(return_values, 1, zeroed_beta_equilibrium_rel);
+
+    return GSL_SUCCESS;
+}
+
+
 double SolveGapEquation(double proton_density,
                         double neutron_density,
                         double proton_fermi_momentum,
                         double neutron_fermi_momentum)
 {
-	const gsl_root_fsolver_type * T	= gsl_root_fsolver_bisection; // Maybe this would be better: gsl_root_fsolver_brent
+    // Maybe this would be better: gsl_root_fsolver_brent
+	const gsl_root_fsolver_type * T	= gsl_root_fsolver_bisection;
 
 	gsl_root_fsolver * s = gsl_root_fsolver_alloc(T);
 
@@ -58,7 +180,10 @@ double SolveGapEquation(double proton_density,
 		
 		x_lower = gsl_root_fsolver_x_lower(s);
 		x_upper = gsl_root_fsolver_x_upper(s);
-	} while(GSL_CONTINUE == gsl_root_test_interval(x_lower, x_upper, CONST_ABS_ERROR_GAP_EQ_SOLVING, CONST_REL_ERROR_GAP_EQ_SOLVING)
+	} while(GSL_CONTINUE == gsl_root_test_interval(x_lower,
+                                                   x_upper,
+                                                   CONST_ABS_ERROR_GAP_EQ_SOLVING,
+                                                   CONST_REL_ERROR_GAP_EQ_SOLVING)
 			&& i <= CONST_MAX_ITERATIONS_GAP_EQ_SOLVING);
 
 	double result = gsl_root_fsolver_root(s);
@@ -163,7 +288,9 @@ double VacuumEnergyDensity()
 {
     double scalar_density_0 = VacuumScalarDensity();
 
-	return VacuumKinecticEnergyDensity() + parameters.bare_mass * scalar_density_0 - parameters.G_S * pow(scalar_density_0, 2.0) * CONST_HBAR_C;
+	return VacuumKinecticEnergyDensity()
+           + parameters.bare_mass * scalar_density_0
+           - parameters.G_S * pow(scalar_density_0, 2.0) * CONST_HBAR_C;
 }
 
 double EnergyDensity(double pressure,
--- src/main.c
+++ src/main.c
@@ -285,4 +285,212 @@ int SolveZeroTemperatureEOS(){
 }
 
 
+int SolveZeroTemperatureStarEOS(){
 
+    // Print name of parametrization
+    if (options.verbose)
+        printf("Calculation performed with %s parameters set.\n", parameters.parameters_set_identifier);
+
+    gsl_vector * barionic_density_vector = gsl_vector_alloc(parameters.points_number);
+    gsl_vector * scalar_density_vector = gsl_vector_alloc(parameters.points_number);
+    gsl_vector * mass_vector = gsl_vector_alloc(parameters.points_number);
+
+	gsl_vector * proton_chemical_potential_vector = gsl_vector_alloc(parameters.points_number);
+	gsl_vector * neutron_chemical_potential_vector = gsl_vector_alloc(parameters.points_number);
+    gsl_vector * kinectic_energy_density_vector = gsl_vector_alloc(parameters.points_number);
+	gsl_vector * termodynamic_potential_vector = gsl_vector_alloc(parameters.points_number);
+
+	gsl_vector * pressure_vector = gsl_vector_alloc(parameters.points_number);
+	gsl_vector * energy_density_vector = gsl_vector_alloc(parameters.points_number);
+
+    /*
+     * Main loop (on barionic density)
+     */
+
+    if (options.verbose)
+        printf("Solving gap equation and equations of state ...\n");
+
+    double barionic_density = parameters.minimum_density;
+
+    // Define the density step. We subtract 1 from the number of points to
+    // make sure that the last point corresponds to maximum_density
+    double density_step = (parameters.maximum_density - parameters.minimum_density)
+                          / (double)(parameters.points_number - 1);
+
+    for (int i = 0; i < parameters.points_number; i++){
+
+        gsl_vector_set(barionic_density_vector, i, barionic_density);
+
+        if (options.verbose){
+            printf("\r\tBarionic density: %f", barionic_density);
+            fflush(stdout);
+        }
+
+		double mass = SolveGapEquation(proton_density,
+                                       neutron_density,
+                                       proton_fermi_momentum,
+                                       neutron_fermi_momentum);
+
+		double total_scalar_density = ScalarDensity(mass, proton_fermi_momentum, parameters.CUTOFF)
+									  + ScalarDensity(mass, neutron_fermi_momentum, parameters.CUTOFF);
+
+
+        gsl_vector_set(scalar_density_vector, i , total_scalar_density);
+        gsl_vector_set(mass_vector, i, mass);
+
+        double proton_chemical_potential =	ProtonChemicalPotential(proton_fermi_momentum,
+																	total_scalar_density,
+																	mass,
+																	barionic_density,
+																	proton_density,
+																	neutron_density);
+
+		gsl_vector_set(proton_chemical_potential_vector, i, proton_chemical_potential);
+
+        double neutron_chemical_potential =	NeutronChemicalPotential(neutron_fermi_momentum,
+																	 total_scalar_density,
+																	 mass,
+																	 barionic_density,
+																	 proton_density,
+																	 neutron_density);
+
+		gsl_vector_set(neutron_chemical_potential_vector, i, neutron_chemical_potential);
+
+		double kinectic_energy_density = KinecticEnergyDensity(mass, proton_fermi_momentum, neutron_fermi_momentum);
+        gsl_vector_set(kinectic_energy_density_vector, i, kinectic_energy_density);
+
+
+		double termodynamic_potential = TermodynamicPotential(total_scalar_density,
+															  barionic_density,
+															  proton_density,
+															  neutron_density,
+															  proton_chemical_potential,
+															  neutron_chemical_potential,
+															  kinectic_energy_density);
+
+		gsl_vector_set(termodynamic_potential_vector, i, termodynamic_potential);
+
+		double pressure = Pressure(termodynamic_potential);
+
+		gsl_vector_set(pressure_vector, i, pressure);
+
+		double energy_density = EnergyDensity(pressure,
+											  proton_chemical_potential,
+											  neutron_chemical_potential,
+											  proton_density,
+											  neutron_density);
+
+		gsl_vector_set(energy_density_vector, i, energy_density);
+
+        // Prepare next iteration
+        barionic_density += density_step;
+    }
+    if (options.verbose)
+        printf("\n"); // As print inside the loop doesn't use new line, we need one now
+
+    // Calculate energy per particle
+    gsl_vector * energy_by_nucleon_vector = VectorNewVectorFromDivisionElementByElement(energy_density_vector,
+                                                                                        barionic_density_vector);
+
+    for (int i = 0; i < energy_by_nucleon_vector->size; i++)
+        gsl_vector_set(energy_by_nucleon_vector, i, gsl_vector_get(energy_by_nucleon_vector, i) - parameters.nucleon_mass);
+
+
+    /*
+     * Save results
+     */
+
+    if (options.verbose)
+        printf("Saving results ...\n");
+
+    if (options.dirs)
+        SetFilePath("output/IR/data/");
+
+    WriteVectorsToFile("mass.dat",
+					   "# barionic density, mass\n",
+					   2,
+					   barionic_density_vector,
+					   mass_vector);
+
+	WriteVectorsToFile("scalar_density.dat",
+					   "# barionic density, scalar density\n",
+					   2,
+					   barionic_density_vector,
+					   scalar_density_vector);
+
+	WriteVectorsToFile("thermodynamic_potential.dat",
+					   "# density, grand canonical potential per unit volume\n",
+					   2,
+					   barionic_density_vector, termodynamic_potential_vector);
+
+	WriteVectorsToFile("proton_chemical_potential.dat",
+					   "# barionic dentisy, proton chemical potential\n",
+					   2,
+					   barionic_density_vector,
+					   proton_chemical_potential_vector);
+
+    WriteVectorsToFile("neutron_chemical_potential.dat",
+                       "# barionic dentisy, neutron chemical potential\n",
+                       2,
+                       barionic_density_vector,
+                       neutron_chemical_potential_vector);
+
+    WriteVectorsToFile("kinectic_energy_density.dat",
+                      "# barionic density, kinectic energy density\n",
+                      2,
+                      barionic_density_vector,
+                      kinectic_energy_density_vector);
+
+    if (options.dirs)
+        SetFilePath("output/Rep_Tsue/data/");
+
+    WriteVectorsToFile("pressure_chem_pot.dat",
+                       "# chemical potential, pressure\n",
+                       2,
+                       proton_chemical_potential_vector,
+                       pressure_vector);
+
+	if (options.dirs)
+        SetFilePath("output/EOS/data/");
+
+	WriteVectorsToFile("pressure.dat",
+					   "# barionic density, pressure\n",
+					   2,
+					   barionic_density_vector,
+					   pressure_vector);
+
+	WriteVectorsToFile("energy_density.dat",
+					   "# barionic density, energy per unit volume\n",
+					   2,
+					   barionic_density_vector,
+					   energy_density_vector);
+
+	WriteVectorsToFile("energy_density_per_particle.dat",
+					   "# energy / barionic density = energy by nucleon \n"
+					   "# barionic density, energy / barionic density\n",
+					   2,
+					   barionic_density_vector, energy_by_nucleon_vector);
+
+    /*
+     * Clean up
+     */
+
+    // Free vectors
+    gsl_vector_free(barionic_density_vector);
+
+    gsl_vector_free(scalar_density_vector);
+    gsl_vector_free(mass_vector);
+
+    gsl_vector_free(proton_chemical_potential_vector);
+    gsl_vector_free(neutron_chemical_potential_vector);
+    gsl_vector_free(termodynamic_potential_vector);
+
+    gsl_vector_free(pressure_vector);
+    gsl_vector_free(energy_density_vector);
+    gsl_vector_free(energy_by_nucleon_vector);
+
+    if (options.verbose)
+        printf("Done!\n");
+
+    return 0;
+}
